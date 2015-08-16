6848c96a86eedd7ca96d37fbacca5fb90b0b675c
teodoramiteva <teodora.miteva93@gmail.com>
2015-08-16 11:12:21 +0300

    test Teodora
    Teodora

--- TestingJoblessDB/src/teams/admin_module/view/TestAdminModule.java
+++ TestingJoblessDB/src/teams/admin_module/view/TestAdminModule.java
@@ -1,9 +1,8 @@
-package teams.admin_module;
+package teams.admin_module.view;
 
 public class TestAdminModule {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
