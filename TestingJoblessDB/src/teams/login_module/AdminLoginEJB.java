4d59dd7536d85d6e6e313655642cc7c9934ab3c6
tpelev <tishkatap@gmail.com>
2015-08-18 11:42:30 +0300

    Working admin Beans

--- TestingJoblessDB/src/teams/login_module/AdminLoginEJB.java
+++ TestingJoblessDB/src/teams/login_module/AdminLoginEJB.java
@@ -2,19 +2,27 @@ package teams.login_module;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import model.Admin;
 
 @Stateless
+@NamedQuery(name = "Login.selectquery", query ="SELECT a FROM Admin a WHERE a.loginName = :loginName")
 public class AdminLoginEJB {
+	
 	@PersistenceContext
 	private EntityManager em;
 	
 	public Admin getAdminUserName(String userName){
-		Query query = em.createQuery("SELECT a FROM Admin a WHERE a.loginName = '"+userName+"'");
-		Admin admin = (Admin) query.getSingleResult();
+		Admin admin;
+		try{
+		Query query = em.createQuery("SELECT a FROM Admin a WHERE a.loginName = :loginName", Admin.class).setParameter("loginName", userName);
+		 admin = (Admin) query.getSingleResult();
+		}catch(Exception e){
+			return null;
+		}
 		return admin;
 	}
 }
--- TestingJoblessDB/src/teams/login_module/LoginAsAdminMenagedBean.java
+++ TestingJoblessDB/src/teams/login_module/LoginAsAdminMenagedBean.java
@@ -2,47 +2,53 @@ package teams.login_module;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import model.Admin;
 
-@ManagedBean(name="LoginAsAdmin")
+@ManagedBean(name = "LoginAsAdminB")
+@SessionScoped
 public class LoginAsAdminMenagedBean {
 	@EJB
 	private AdminLoginEJB admin;
-	
-	private String userName;
-	private String pass1;
-	
-	
-	public void loginAdmin(){
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA");
-		String userName = this.getUserName();
-		String pass1 = this.getPass1();
+
+	public String adminUserName;
+	private String pass;
+
+	public void loginAdmin() {
 		
+		String userName = this.getAdminUserName();
+		
+		String pass1 = this.getPass();
+		System.out.println("entered pass" + pass1);
+
 		try {
-			Admin test=admin.getAdminUserName(userName);
-			if(pass1.equals(test.getLoginPassword())){
+			Admin test = admin.getAdminUserName(adminUserName);
+			if (pass1.equals(test.getLoginPassword())) {
 				System.out.println("Success!! Welcome admin!");
 			}
-			
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPass1() {
-		return pass1;
-	}
-	public void setPass1(String pass1) {
-		this.pass1 = pass1;
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
-	
-	
+	public String getAdminUserName() {
+		return adminUserName;
+		
+	}
+
+	public void setAdminUserName(String name) {
+		this.adminUserName = name;
+		
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
 }
